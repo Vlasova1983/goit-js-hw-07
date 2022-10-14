82213c43b60cff8df769b1102e17dbe849c44b85
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-14 16:06:43 +0300

    Update 02-lightbox.js
    ---

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,7 @@ const imagePalette = document.querySelector(".gallery");
   const makeImage = ({preview, original, description}) => {  
       return `
       <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
+      <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>        
       `
     }
@@ -15,16 +15,13 @@ const makeImagas = galleryItems
     .join('');
     
 imagePalette.insertAdjacentHTML('beforeend', makeImagas);
-imagePalette.addEventListener('click', onClick); 
+imagePalette.addEventListener('click', onGalleryClick);
+function onGalleryClick(evt) {
+      evt.preventDefault();
+  }
 
-function onClick(evt) {
- if (evt.target.nodeName !== "IMG") {   
-   return;
- };
-const lightBox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionsDelay: 250,
-});
-lightBox.open();
-}
+  const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
 
